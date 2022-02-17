46fa9a980ebf5815105e970797480959a0aeca3a
Delsa <51116291+Edelveysa@users.noreply.github.com>
2022-02-17 15:35:52 +0300

    Add files via upload

---
+++ js/func.js
@@ -0,0 +1,110 @@
+let x, y, r;
+let errorMessage = "";
+const maxLength = 7;
+
+function isNumber(input) {
+    return !isNaN(parseFloat(input)) && isFinite(input);
+}
+
+function addToErrorMessage(errorDesc) {
+    errorMessage += (errorDesc + "\n");
+}
+
+function hasProperLength(input) {
+    return input.length <= maxLength;
+}
+
+function validateX() {
+    let XButtons = document.querySelectorAll("input[name=x]");
+
+    XButtons.forEach(function (button) {
+        console.log(button.value);
+        if (button.checked) {
+            x = button.value;
+            console.log("success");
+        }
+    });
+
+    if (x === undefined) {
+        addToErrorMessage("Выберите X.");
+        console.log("check x");
+        return false;
+    }
+    return true;
+}
+
+function validateY() {
+    y = document.querySelector("input[id=yCoordinate]").value.replace(",", ".");
+    if (y === undefined) {
+        addToErrorMessage("Поле Y не заполнено");
+        return false;
+    }
+    if (!isNumber(y)) {
+        addToErrorMessage("Y должен быть числом от -3 до 3!");
+        return false;
+    }
+    if (!hasProperLength(y)) {
+        addToErrorMessage(`Длина числа должна быть не более ${maxLength} символов`);
+        return false;
+    }
+    if (!((y > -3) && (y < 3))) {
+        addToErrorMessage("Нарушена область допустимых значений Y (-3; 3)");
+        return false;
+    }
+    return true;
+}
+
+function validateR() {
+    r = document.querySelector("input[id=rCoordinate]").value.replace(",", ".");
+    if (r === undefined) {
+        addToErrorMessage("Поле R не заполнено");
+        return false;
+    }
+    if (!isNumber(r)) {
+        addToErrorMessage("R должен быть числом от 1 до 4!");
+        return false;
+    }
+    if (!hasProperLength(r)) {
+        addToErrorMessage(`Длина числа должна быть не более ${maxLength} символов`);
+        return false;
+    }
+    if (!((r > 1) && (r < 4))) {
+        addToErrorMessage("Нарушена область допустимых значений R (1; 4)");
+        return false;
+    }
+    return true;
+}
+
+
+function submit() {
+    if (validateX() & validateY() & validateR()) {
+        $.get("php/main.php", {
+            'x': x,
+            'y': y,
+            'r' : r,
+            'timezone': new Date().getTimezoneOffset()
+        }).done(function(PHP_RESPONSE) { // do when success callback is received
+            let result = JSON.parse(PHP_RESPONSE); // take array with results
+            if (!result.isValid) {
+                addToErrorMessage("Request is not valid. Try refreshing the page");
+                return;
+            }
+            let newRow = result.isBlueAreaHit ? '<tr class="hit-yes">' : '<tr class="hit-no">';
+            newRow += '<td>' + result.x + '</td>';
+            newRow += '<td>' + result.y + '</td>';
+            newRow += '<td>' + result.r + '</td>';
+            newRow += '<td>' + result.userTime + '</td>';
+            newRow += '<td>' + result.execTime + '</td>';
+            newRow += '<td>' + (result.isBlueAreaHit ? '<img src="../img/tick.png" alt="Да" class="yes-no-marker">' : '<img src="../img/cross.png" alt="Нет" class="yes-no-marker">') + '</td>';
+            $('#result-table tr:first').after(newRow);
+            document.getElementById("result-table").style.backgroundColor = `rgba(250, 235, 215, ${Math.random() * 0.6 + 0.2})`;
+        }).fail(function (error) {
+            addToErrorMessage(error);
+        });
+    }
+
+    if (!(errorMessage === "")) {
+        alert(errorMessage);
+        errorMessage = "";
+    }
+}
---
+++ php/main.php
@@ -0,0 +1,83 @@
+<?php
+
+function validateX($inp) {
+    return isset($inp) && is_numeric($inp) && $inp >= 1 && $inp <=3 && $inp * 2 == intval($inp * 2);
+}
+
+function validateY($inp)
+{
+    if (!isset($inp)) return false;
+
+    $Y_MIN = -3;
+    $Y_MAX = 3;
+
+    $y_num = str_replace(",", ".", $inp);
+    return is_numeric($y_num) && $Y_MIN < $y_num && $y_num < $Y_MAX && strlen($inp) <= 7;
+}
+
+function validateR($inp)
+{
+    if (!isset($inp)) return false;
+
+    $R_MIN = 1;
+    $R_MAX = 4;
+
+    $r_num = str_replace(",", ".", $inp);
+    return is_numeric($r_num) && $R_MIN < $r_num && $r_num < $R_MAX && strlen($inp) <= 7;
+}
+
+function validateTimezone($inp) {
+    return isset($inp) && is_numeric($inp) && abs($inp) <= 24 * 60;
+}
+
+function isSquareHit($x, $y, $r) {
+    return ($x < 0 && $y > 0 && $x > -$r && $y < $r);
+}
+
+function isTriangleHit($x, $y, $r) {
+    $hypotenuse = -1/2 * $x - $r/2;
+    return ($x < 0 && $y < 0 && $y > $hypotenuse);
+
+}
+
+function isCircleHit($x, $y, $r)
+{
+    $isInsideCircle = pow($x, 2) + pow($y, 2) < pow($r, 2);
+    return ($x > 0 && $y < 0 && $isInsideCircle);
+}
+
+function isBlueAreaHit($x, $y, $r) {
+    return isCircleHit($x, $y, $r) || isTriangleHit($x, $y, $r) || isSquareHit($x, $y, $r);
+}
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
+$x = $_GET['x'];
+$y = $_GET['y'];
+$r = $_GET['r'];
+$timezone = $_GET['timezone'];
+
+
+
+    $isValid = validateR($r) && validateX($x) && validateY($y) && validateTimezone($timezone);
+    $isBlueAreaHit = NULL;
+    $userTime = NULL;
+    $timePassed = NULL;
+    if ($isValid) {
+        $isBlueAreaHit = isBlueAreaHit($x, $y, $r);
+        $userTime = @date('H:i:s', time() - $timezone * 60);
+        $timePassed = round(microtime(true) - $_SERVER['REQUEST_TIME_FLOAT'], 4);
+    }
+    $response[] = array(
+        "isValid" => $isValid,
+        "isBlueAreaHit" => $isBlueAreaHit,
+        "userTime" => $userTime,
+        "execTime" => $timePassed,
+        "x" => $x,
+        "y" => $y,
+        "r" => $r);
+echo json_encode($response);
+
